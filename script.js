66578c5f230e2821787b2575ac7c294af206f32c
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2024-02-01 17:10:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,12 +39,12 @@ let getbase=()=>{
 
 
 //Player Openers (diff. func. coding intentionally)
-let openplyr=()=> { window.open('https://videoplyr.sh20raj.repl.co/plyr.html?id='+base)}
-let openfluid=()=> { window.open('https://videoplyr.sh20raj.repl.co/fluid.html?id='+base)}
-let openafterglow=()=> { window.open('https://videoplyr.sh20raj.repl.co/afterglow.html?id='+base)}
-let openmediaelements=()=> { window.open('https://videoplyr.sh20raj.repl.co/mediaelements.html?id='+base)}
-let openvlitejs=()=> { window.open('https://videoplyr.sh20raj.repl.co/vlitejs.html?id='+base)}
-let opensp=()=> { window.open('https://videoplyr.sh20raj.repl.co/sopplayer.html?id='+base)}
+let openplyr=()=> { window.open('https://sh20raj.github.io/VideoPlyr/plyr.html?id='+base)}
+let openfluid=()=> { window.open('https://sh20raj.github.io/VideoPlyr/fluid.html?id='+base)}
+let openafterglow=()=> { window.open('https://sh20raj.github.io/VideoPlyr/afterglow.html?id='+base)}
+let openmediaelements=()=> { window.open('https://sh20raj.github.io/VideoPlyr/mediaelements.html?id='+base)}
+let openvlitejs=()=> { window.open('https://sh20raj.github.io/VideoPlyr/vlitejs.html?id='+base)}
+let opensp=()=> { window.open('https://sh20raj.github.io/VideoPlyr/sopplayer.html?id='+base)}
 
 
 let pmsg = 'Copy Embed Code' ;
@@ -52,7 +52,7 @@ let pmsg = 'Copy Embed Code' ;
 let embedplyr=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://videoplyr.sh20raj.repl.co/plyr.html?id=${base}" 
+src="https://sh20raj.github.io/VideoPlyr/plyr.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -60,7 +60,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedfluid=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://videoplyr.sh20raj.repl.co/fluid.html?id=${base}" 
+src="https://sh20raj.github.io/VideoPlyr/fluid.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -68,7 +68,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedafterglow=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://videoplyr.sh20raj.repl.co/afterglow.html?id=${base}" 
+src="https://sh20raj.github.io/VideoPlyr/afterglow.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -76,7 +76,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedmediaelements=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://videoplyr.sh20raj.repl.co/mediaelements.html?id=${base}" 
+src="https://sh20raj.github.io/VideoPlyr/mediaelements.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`)
@@ -84,7 +84,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedvlitejs=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://videoplyr.sh20raj.repl.co/vlitejs.html?id=${base}" 
+src="https://sh20raj.github.io/VideoPlyr/vlitejs.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`);
@@ -92,7 +92,7 @@ gyroscope; picture-in-picture" allowfullscreen>
 let embedsp=()=> {
   prompt(pmsg,`<iframe width="560" height="315" 
 scrolling="no"
-src="https://videoplyr.sh20raj.repl.co/sopplayer.html?id=${base}" 
+src="https://sh20raj.github.io/VideoPlyr/sopplayer.html?id=${base}" 
 frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
 gyroscope; picture-in-picture" allowfullscreen>
 </iframe>`);
@@ -101,11 +101,11 @@ gyroscope; picture-in-picture" allowfullscreen>
 
 
 let iframe=()=> {
-  s('afterglow').src= 'https://videoplyr.sh20raj.repl.co/afterglow.html?id='+base;
-  s('fluid').src='https://videoplyr.sh20raj.repl.co/fluid.html?id='+base;
-  s('plyr').src='https://videoplyr.sh20raj.repl.co/plyr.html?id='+base;
-  s('vlitejs').src='https://videoplyr.sh20raj.repl.co/vlitejs.html?id='+base;
-  s('mediaelements').src='https://videoplyr.sh20raj.repl.co/mediaelements.html?id='+base;
-    s('sopplayer').src='https://videoplyr.sh20raj.repl.co/sopplayer.html?id='+base;
+  s('afterglow').src= 'https://sh20raj.github.io/VideoPlyr/afterglow.html?id='+base;
+  s('fluid').src='https://sh20raj.github.io/VideoPlyr/fluid.html?id='+base;
+  s('plyr').src='https://sh20raj.github.io/VideoPlyr/plyr.html?id='+base;
+  s('vlitejs').src='https://sh20raj.github.io/VideoPlyr/vlitejs.html?id='+base;
+  s('mediaelements').src='https://sh20raj.github.io/VideoPlyr/mediaelements.html?id='+base;
+    s('sopplayer').src='https://sh20raj.github.io/VideoPlyr/sopplayer.html?id='+base;
   
 }
